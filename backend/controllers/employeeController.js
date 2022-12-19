1a8884e4c15521dfb6c373cbc0425d52a56d85e0
Therese Linden <theresemlinden@gmail.com>
2022-12-19 21:39:53 +0100

    added get route for all json data

--- backend/controllers/employeeController.js
+++ backend/controllers/employeeController.js
@@ -1,8 +1,21 @@
 const jsonData = require('../employees.json');
+const fs = require('fs');
+
+//https://blog.logrocket.com/error-handling-node-js/
+const util = require('util');
+
+const readFile = util.promisify(fs.readFile);
+const dataPath = 'backend/employees.json';
 
 // GET all employees
 const getEmployees = async (req, res) => {
-  res.send('Hello from All Employees route');
+  try {
+    const result = await readFile(dataPath);
+    res.status(200).send(result);
+  } catch (err) {
+    res.status(400);
+    throw new Error(`Error: ${err}`);
+  }
 };
 
 const createEmployee = async (req, res) => {
