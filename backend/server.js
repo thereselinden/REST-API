0af0b22979782134cb0609affb280b64eeeac7f2
Therese Linden <theresemlinden@gmail.com>
2022-12-19 20:09:46 +0100

    first commit

---
+++ backend/server.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const dotenv = require('dotenv').config();
+const port = process.env.PORT || 3000;
+const app = express();
+
+app.listen(port, () => console.log(`Server running on port: ${port}`));
